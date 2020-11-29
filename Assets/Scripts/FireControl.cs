df6ec19a9a289ddf57f7da1662e9809be026ff8e
Marcin <marcin.karczewski67@gmail.com>
2020-11-29 15:09:35 +0100

    wstepne assety

---
+++ Assets/Scripts/FireControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireControl : MonoBehaviour
+{
+    public FireOn[] ControlledFires;
+
+    private void OnFire()
+    {
+        foreach (var fire in ControlledFires)
+        {
+            fire.ChangeFireState();
+        }
+    }
+}
---
+++ Assets/Scripts/FireOn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireOn : MonoBehaviour
+{
+    private ParticleSystem p_system;
+    void Awake()
+    {
+        p_system = GetComponentInChildren<ParticleSystem>();
+    }
+
+    public void ChangeFireState()
+    {
+        if (p_system.isPlaying)
+        {
+            p_system?.Stop(true);
+        }
+        else
+        {
+            p_system?.Play(true);
+        }
+    }
+}
